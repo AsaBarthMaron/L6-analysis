c767fb6d149632e44d76131000a569a0bc4695f0
Asa Barth-Maron <asabarthmaron@gmail.com>
2015-02-14 12:57:58 -0500

    p

--- quantifyGain.m
+++ quantifyGain.m
@@ -3,6 +3,8 @@ function quantifyGain(indices,toneResponses,startingBins)
     [masterDataExp,slaveDataExp] = laserDelay_tuningCurve_secondAnalysis(indices,toneResponses,startingBins,0)
     
     for k = indices
-        
+        for i = toneResponses{k}
+        	gainQuant{k,i} = (mean(slaveDataExp)-mean(masterDataExp{}))/(mean(slaveDataExp)+mean(masterDataExp{}))
+        end
     end
    
